221fa6c5bb05dac4b4e6b929b162baaca246e53e
Sebastian <mrseb10@gmail.com>
2024-04-19 21:15:43 +0200

    Defining Shift Model

---
+++ ShiftsLogger.BBualdo/Models/Shift.cs
@@ -0,0 +1,9 @@
+﻿namespace ShiftsLogger.BBualdo.Models;
+
+public class Shift
+{
+  public int ShiftId { get; set; }
+  public string? EmployeeName { get; set; }
+  public DateTime StartDate { get; set; }
+  public DateTime EndDate { get; set; }
+}
