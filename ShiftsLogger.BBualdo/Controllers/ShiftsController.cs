c79ca94b62745cf58f7893fe02bd2bbe6e3c11ce
Sebastian <mrseb10@gmail.com>
2024-04-20 05:17:27 +0200

    API Controller with GetShifts

---
+++ ShiftsLogger.BBualdo/Controllers/ShiftsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShiftsLogger.BBualdo.Models;
+
+namespace ShiftsLogger.BBualdo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShiftsController : ControllerBase
+{
+  private readonly ShiftsContext _context;
+
+  public ShiftsController(ShiftsContext context)
+  {
+    _context = context;
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<Shift>>> GetShifts()
+  {
+    if (_context.Shifts == null) return NotFound();
+
+    return await _context.Shifts.ToListAsync();
+  }
+}
