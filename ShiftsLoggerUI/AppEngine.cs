9084e4860c6af9b05a1bc44c3ccb61ba84afcc26
Sebastian <mrseb10@gmail.com>
2024-04-20 15:59:22 +0200

    UI: Main Menu

---
+++ ShiftsLoggerUI/AppEngine.cs
@@ -0,0 +1,37 @@
+﻿using Spectre.Console;
+
+namespace ShiftsLoggerUI;
+
+internal class AppEngine
+{
+  internal bool IsRunning { get; set; }
+
+  public AppEngine()
+  {
+    IsRunning = true;
+  }
+
+  internal void MainMenu()
+  {
+    ConsoleEngine.ShowTitle();
+
+    string choice = ConsoleEngine.GetChoiceSelector("What you would like to do?", ["Show Shifts", "Create Shift", "Update Shift", "Delete Shift", "Quit"]);
+
+    switch (choice)
+    {
+      case "Show Shifts":
+        break;
+      case "Create Shift":
+        break;
+      case "Update Shift":
+        break;
+      case "Delete Shift":
+        break;
+      case "Quit":
+        AnsiConsole.Clear();
+        AnsiConsole.Markup("[cyan1]GOODBYE[/]");
+        IsRunning = false;
+        break;
+    }
+  }
+}
---
+++ ShiftsLoggerUI/ConsoleEngine.cs
@@ -0,0 +1,28 @@
+﻿using Spectre.Console;
+
+namespace ShiftsLoggerUI;
+
+internal static class ConsoleEngine
+{
+  internal static string GetChoiceSelector(string title, string[] choices)
+  {
+    SelectionPrompt<string> prompt = new SelectionPrompt<string>()
+                                     .Title(title)
+                                     .AddChoices(choices)
+                                     .HighlightStyle(Color.Cyan1);
+
+    string choice = AnsiConsole.Prompt(prompt);
+
+    return choice;
+  }
+
+  internal static void ShowTitle()
+  {
+    AnsiConsole.Clear();
+
+    Rule rule = new("Shifts Logger");
+    rule.Centered().HeavyBorder().Style = new Style(Color.Cyan1);
+
+    AnsiConsole.Write(rule);
+  }
+}
